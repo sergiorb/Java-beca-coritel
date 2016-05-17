bfe85dbae85e91c570623d8e6f000d9ed2cdc48f
Sergio Romero Barra <s.romerobarra@gmail.com>
2016-05-17 15:02:56 +0200

    Added params to showMessage method in 'LibroCalificaciones.java'.

--- src/com/sergiorb/java_beca_coritel/libro_de_calificaciones/LibroCalificaciones.java
+++ src/com/sergiorb/java_beca_coritel/libro_de_calificaciones/LibroCalificaciones.java
@@ -9,7 +9,7 @@ package com.sergiorb.java_beca_coritel.libro_de_calificaciones;
 
 public class LibroCalificaciones {
 	
-	private String welcomeMessage = "Hello there!"; // Welcome message var.
+	private String welcomeMessage = "Welcome to"; // Welcome message var.
 	private int welcomeMessageMinLength = 8; // welocome message min length var
 
 	// welcomeMessage getter.
@@ -31,8 +31,14 @@ public class LibroCalificaciones {
 	} // end wellcomeMessage setter
 
 	// Prints showMessage var.
-	public void showMessage() {
-		System.out.println(this.welcomeMessage.toString());
+	public void showMessage(String courseName) {
+		
+		// Checks if courseName have 1 or more chars.
+		if(courseName.length() > 0) {
+			System.out.printf("%s %s course.\n", this.welcomeMessage, courseName);
+		} else {
+			System.out.println("You need to enter one ore more characters");
+		}
+		
 	} // end showMessage
-
 }
--- src/com/sergiorb/java_beca_coritel/libro_de_calificaciones/PruebaLibroCalificaciones.java
+++ src/com/sergiorb/java_beca_coritel/libro_de_calificaciones/PruebaLibroCalificaciones.java
@@ -7,18 +7,38 @@
 
 package com.sergiorb.java_beca_coritel.libro_de_calificaciones;
 
+import java.util.Scanner;
+
 public class PruebaLibroCalificaciones {
 
 	// Main method
 	public static void main(String[] args) {
 		
-		LibroCalificaciones book = new LibroCalificaciones(); // Object instanciation
+		Scanner scan = new Scanner(System.in); // Scanner object instanciation
 		
-		book.showMessage();
-		book.setWelcomeMessage("Hello over there!!");
-		book.showMessage();
-		book.setWelcomeMessage("Hello!!");
-		book.showMessage();
+		try {
+			
+			LibroCalificaciones book = new LibroCalificaciones(); // Object instanciation
+			
+			book.showMessage("");
+			book.showMessage("Python"); // Calls showMessage
+			book.setWelcomeMessage("Welcome to the scary"); // Sets welcomeMessage property.
+			book.showMessage("HTML5"); // Calls showMessage
+			book.setWelcomeMessage("Welcome to me mega scary"); // Sets welcomeMessage property.
+			book.showMessage("Scala");
+				
+			System.out.println("Introduce course name:");
+			String courseName = scan.nextLine(); // reads course name
+			
+			book.showMessage(courseName); // Sets welcomeMessage property.
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			
+			scan.close(); // Close scan stream object.
+			System.out.println("Program finished!");
+		}
 	}
 
 }
