7abd65f735d32665feac9b442b1e863879fd584c
sergiorb <s.romerobarra@gmail.com>
2016-05-28 00:01:30 +0200

    Documented enumbook.

--- src/com/sergiorb/java_beca_coritel/ud8/enumbook/Book.java
+++ src/com/sergiorb/java_beca_coritel/ud8/enumbook/Book.java
@@ -1,14 +1,25 @@
+/**
+*	@file Book.java
+*	@author Sergio Romero Barra
+*
+*	Defines a enum Book class that stores books.
+*/
+
 package com.sergiorb.java_beca_coritel.ud8.enumbook;
 
+/** @class declaration */
 public enum Book {
 
+	// Constants variables
 	JHTP( "Java How to Program" , "2012" ),
 	CHTP( "C How to Program" , "2007" ),
 	IW3HTP( "Internet & World Wide Web How to Program" , "2008" ),
 	CPPHTP( "C++ How to Program" , "2012" ),
-	VBHTP( "Visual Basic: 2010 How to Program" , " 2011" ),
+	VBHTP( "Visual Basic: 2010 How to Program" , "2011" ),
 	CSHARPHTP( "Visual Cll 2010 How to Program", "2011" );
 	
+	// enum object instance variables that match field
+	// defined above.
 	private final String title;
 	private final String copyrightYear;
 	
--- src/com/sergiorb/java_beca_coritel/ud8/enumbook/TestBook.java
+++ src/com/sergiorb/java_beca_coritel/ud8/enumbook/TestBook.java
@@ -1,18 +1,30 @@
+/**
+*	@file TestBook.java
+*	@author Sergio Romero Barra
+*
+*	Shows enum Book class usage.
+*/
+
 package com.sergiorb.java_beca_coritel.ud8.enumbook;
 
 import java.util.EnumSet;
 
+/** @class main */
 public class TestBook {
 
+	/** @method main */
 	public static void main(String[] args) {
 		
+		
 		System.out.println("Show all books:\n");
-		for (Book book : Book.values()) {
+		for (Book book : Book.values()) { // Retrieves all enum constants 
 			System. out. printf( "%-10s%-45s%s\n", book , book.getTitle(), book.getCopyrightYear());
 		}
 
 		System.out.println("\nShow a constants range:\n");
-		for (Book book: EnumSet.range(Book.JHTP, Book.CPPHTP)) {
+		// Retrieves all values in Book class between JHTP and CPPHTP constant.
+		// This is possible due its(Book enum) inheritance from set class .
+		for (Book book: EnumSet.range(Book.JHTP, Book.CPPHTP)) { 
 			System. out. printf( "%-10s%-45s%s\n", book , book.getTitle(), book.getCopyrightYear());
 		}
 	}
